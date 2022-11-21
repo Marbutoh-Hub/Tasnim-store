06d0eb07c4bcdd544b0b9ff904c713ae2c70e12d
Giri Gimnastiar <girigimnastiarwork@gmail.com>
2022-11-22 01:33:18 +0700

    membuat design dashboard

--- component-dashboard/index.php
+++ component-dashboard/index.php
@@ -1,3 +1,48 @@
 <div>
-    <h3>Dashboard</h3>
+    <div class="dashboard-main">
+        <div class="side-left">
+            <div class="icon-dashboard">
+                <div class="header-dashboard">
+                    <img  src="images/logo-login.png" alt="">
+                    <h3>Tasnim Store</h3>
+                </div>
+            </div>
+            <div class="nav-menu">
+                <dl id="list">
+                    <dt>
+                       <img class="icon" src="icon/dashboard.svg" alt="">
+                       <a href="#">Dashboard</a>
+                    </dt>
+                    <dt>
+                        <img class="icon" src="icon/master.svg" alt="">
+                        <a href="#">Master</a>
+                    </dt>
+                    <dt>
+                        <img class="icon" src="icon/customer.svg" alt="">
+                        <a href="#">Customer</a>
+                    </dt>
+                    <dt>
+                        <img class="icon" src="icon/product.svg" alt="">
+                        <a href="#">Produk</a>   
+                    </dt>
+                    <dt>
+                        <img class="icon" src="icon/transaksi.svg" alt="">
+                        <a href="#">Transaksi</a>
+                    </dt>
+                    <dt>
+                        <img class="icon" src="icon/laporan.svg" alt="">
+                        <a href="#">Laporan</a>
+                    </dt>
+                </dl>
+            </div>
+            <div class="footer-dashboard">
+            </div>
+        </div>
+        <div class="dashboard-component">
+
+        </div>
+        <div class="side-right">
+
+        </div>
+    </div>
 </div>
