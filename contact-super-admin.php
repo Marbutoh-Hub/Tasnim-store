0e1e5bb16a61413da7899f674fb2a88069b47c40
Giri Gimnastiar <girigimnastiarwork@gmail.com>
2022-11-21 11:35:41 +0700

    membuat repositori baru untuk toko online tasnim-store

---
+++ contact-super-admin.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styleinfo.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <script src="js/dom.js"></script>
+    <script src="https://kit.fontawesome.com/227adaf680.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+    <title>Contact super admin</title>
+</head>
+<body>
+    <div id="main-container">
+            <div id="container">
+                <div class="header">
+                    <div id="super-admin-img">
+                        <img src="images/super-admin.jpg" alt="">
+                    </div>
+                    <div id="title">
+                        <h3>Bagas Wicaksono</h5>
+                        <h6>Super Admin</h6>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="section">
+                        <p>
+                            Silahkan anda hubungi super admin jika membutuhkan akun baru untuk menjadi admin atau menjadi pengelola website Tasnim Store. 
+                        </p>
+                    </div>
+                    <hr>
+                    <div class="contact">
+                        <span id="whatsapp">
+                            <a href="https://wa.me/6283149196896?text=Hello pak Cahyo">
+                            <i class="fa-brands fa-whatsapp"></i>
+                            </a>
+                        </span>
+                        <span id="email">
+                            <a href="mailto:girigimnastiar93@gmail.com?subject=%20Buat%20Akun%20Baru">
+                                <i class="fa-brands fa-google"></i>
+                            </a>
+                        </span>
+                        <span id="telephone">
+                            <a href="tel:+62895325255129" style="color: black;">
+                                <i class="fa-solid fa-phone"></i>
+                            </a>
+                        </span>
+                    </div>
+                    <div class="footer">
+                        <a href="index.php">Back</a>
+                    </div>
+                </div>
+            </div>
+    </div>
+</body>
+</html>
---
+++ js/dom.js
@@ -0,0 +1,10 @@
+// var btnSubmit = document.getElementById("test");
+
+// btnSubmit.addEventListener("click", function () {
+//   console.log("bangsat");
+// });
+// const btnWhatsApp = document.getElementById("whatsapp");
+
+// btnWhatsApp.addEventListener("click", function () {
+//   alert("anjing");
+// });
