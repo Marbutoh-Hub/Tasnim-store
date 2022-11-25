ec15dce7fc93dd4723bab1d3cf714c7380f2e7e2
Giri Gimnastiar <girigimnastiarwork@gmail.com>
2022-11-26 04:12:28 +0700

    memperbaiki list menu navigasi

--- component-dashboard/index.php
+++ component-dashboard/index.php
@@ -3,34 +3,41 @@
         <div class="side-left">
             <div class="icon-dashboard">
                 <div class="header-dashboard">
-                    <img  src="images/logo-login.png" alt="">
+                    <img src="images/logo-login.png" alt="">
                     <h3>Tasnim Store</h3>
                 </div>
             </div>
             <div class="nav-menu">
                 <dl id="list">
                     <dt>
-                       <img class="icon dashboard-icon" src="icon/dashboard.svg" alt="">
-                       <a id="dashboard" href="#">Dashboard</a>
+                        <img class="icon dashboard-icon" src="icon/dashboard-icon.svg" alt="">
+                        <a id="dashboard" href="#">Dashboard</a>
                     </dt>
                     <dt>
-                        <img class="icon master-icon" src="icon/master.svg" alt="">
+                        <img class="icon master-icon" src="icon/master-icon.svg" alt="">
                         <a id="master" href="#">Master</a>
                     </dt>
+                    <div id="master-detail">
+                        <ul>
+                            <li><a href="">Master Customer</a></li>
+                            <li><a href="">Master Produk</a></li>
+                            <li><a href="">Master Admin</a></li>
+                        </ul>
+                    </div>
                     <dt>
-                        <img class="icon customer-icon" src="icon/customer.svg" alt="">
+                        <img class="icon customer-icon" src="icon/customer-icon.svg" alt="">
                         <a id="customer" href="#">Customer</a>
                     </dt>
                     <dt>
-                        <img class="icon produk-icon" src="icon/product.svg" alt="">
-                        <a id="produk" href="#">Produk</a>   
+                        <img class="icon produk-icon" src="icon/produk-icon.svg" alt="">
+                        <a id="produk" href="#">Produk</a>
                     </dt>
                     <dt>
-                        <img class="icon transaksi-icon" src="icon/transaksi.svg" alt="">
+                        <img class="icon transaksi-icon" src="icon/transaksi-icon.svg" alt="">
                         <a id="transaksi" href="#">Transaksi</a>
                     </dt>
                     <dt>
-                        <img class="icon laporan-icon" src="icon/laporan.svg" alt="">
+                        <img class="icon laporan-icon" src="icon/laporan-icon.svg" alt="">
                         <a id="laporan" href="#">Laporan</a>
                     </dt>
                 </dl>
@@ -39,7 +46,6 @@
             </div>
         </div>
         <div class="dashboard-component">
-
         </div>
         <div class="side-right">
 
@@ -49,26 +55,31 @@
 
 <script>
     let targetSelected;
-    $("dl dt a").on('click',function(e){
-        this.targetSelect   = e.target.id;
-        idList              = $("dl dt a");
-        imgList             = $("dl dt img");
-        imgSelected         = this.targetSelect+"-icon";
+    $("dl dt a").on('click', function(e) {
+        this.targetSelect = e.target.id;
+        idList = $("dl dt a");
+        imgList = $("dl dt img");
+        imgSelected = this.targetSelect + "-icon";
         console.log(imgSelected);
         for (let index = 0; index < idList.length; index++) {
             if (idList[index].id === this.targetSelect) {
-                $('#'+idList[index].id).css('color','blue');    
-            }else{
-                $('#'+idList[index].id).css('color','black');
+                $('#' + idList[index].id).css('color', '#FF00FF');
+                if (idList[index].id === 'master') {
+                    $('#master-detail').toggle();
+                } else {
+                    $('#master-detail').hide();
+                }
+            } else {
+                $('#' + idList[index].id).css('color', 'black');
             }
         }
 
         for (let index = 0; index < imgList.length; index++) {
             console.log(imgList[index].classList[1])
             if (imgList[index].classList[1] === imgSelected) {
-                $('.'+imgList[index].classList[1]).css('backgroundColor','blue');  
-            }else{
-                $('.'+imgList[index].classList[1]).css('backgroundColor','white');  
+                $('.' + imgList[index].classList[1]).attr('src', 'icon/' + imgSelected + '-onselect.svg');
+            } else {
+                $('.' + imgList[index].classList[1]).attr('src', 'icon/' + imgList[index].classList[1] + '.svg');
             }
         }
     });
