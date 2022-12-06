2ef2024df337de466f1043ecc9f11fdcbb54cb37
Giri Gimnastiar <girigimnastiarwork@gmail.com>
2022-12-06 14:24:55 +0700

    menambahkan list notes dan animasi toggle

--- component-dashboard/index.php
+++ component-dashboard/index.php
@@ -19,9 +19,9 @@
                     </dt>
                     <div id="master-detail">
                         <ul>
-                            <li><a href="">Master Customer</a></li>
-                            <li><a href="">Master Produk</a></li>
-                            <li><a href="">Master Admin</a></li>
+                            <li><a id="master-customer" href="#">Master Customer</a></li>
+                            <li><a id="master-produk" href="#">Master Produk</a></li>
+                            <li><a id="master-admin" href="#">Master Admin</a></li>
                         </ul>
                     </div>
                     <dt>
@@ -43,32 +43,123 @@
                 </dl>
             </div>
             <div class="footer-dashboard">
+                <div id="notes">
+                    <h6>Create notes :</h6>
+                    <div class="d-grid gap-2 section-notes">
+                        <button style="padding:8px; text-align:left; color:black; " type="button" id="button-notes" class="btn btn-outline-secondary btn-sm" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                            <img style="margin-top:-3px; " class="icon" src="icon/notes-icon.svg" alt="">
+                            New notes
+                        </button>
+                    </div>
+                    <div class="list-notes">
+                        <div class="list-group mt-2" id="notif-notes">
+                            <button type="button" class="list-group-item list-group-item-action">
+                                A second item
+                                <span class="notif-notes">
+                                    <img src="icon/notif-icon.svg" alt="">
+                                    <!-- <img src="icon/icon-check-notif.svg" alt=""> -->
+                                </span>
+                            </button>
+                            <button type="button" class="list-group-item list-group-item-action">
+                                A second item 2
+                                <span class="notif-notes">
+                                    <!-- <img src="icon/notif-icon.svg" alt=""> -->
+                                    <img src="icon/icon-check-notif.svg" alt="">
+                                </span>
+                            </button>
+                            <button type="button" class="list-group-item list-group-item-action">
+                                A third button item
+                                <span class="notif-notes">
+                                    <!-- <img src="icon/notif-icon.svg" alt=""> -->
+                                    <img src="icon/icon-check-notif.svg" alt="">
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="dashboard-component">
+
         </div>
         <div class="side-right">
 
         </div>
     </div>
+
 </div>
 
+<!-- Modal Notes -->
+<div id="modal-notes">
+    <div class="card-notes">
+        <div class="header-notes">
+            <h5>My notes</h5>
+        </div>
+        <div class="content-notes">
+            <form class="mt-3" action="">
+                <div class="row g-3 align-items-center">
+                    <div class="col-auto">
+                        <label for="tanggal_notes" class="col-form-label">Tanggal</label>
+                    </div>
+                    <div class="col-auto">
+                        <input type="date" id="tanggal_notes" class="form-control form-control-sm">
+                    </div>
+                </div>
+                <div class="mb-3 mt-4">
+                    <label for="new_note" class="form-label">Create notes :</label>
+                    <textarea class="form-control" placeholder="create new notes here" id="new_note" style=" height: 100px"></textarea>
+                    </textarea>
+                </div>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end mt-4">
+                    <button id="cancel-modal" class="btn btn-warning me-md-2" type="button">Cancel</button>
+                    <button class="btn btn-info" type="submit">Created</button>
+                </div>
+            </form>
+        </div>
+        <div class="footer-notes">
+
+        </div>
+
+    </div>
+</div>
+
+
 <script>
+    $(document).ready(function() {
+        $('#modal-notes').hide();
+    })
     let targetSelected;
+    let masterSelected;
+
+    // function merubah warna pada master
+    function detailList(ml, TargetList) {
+        masterList = $('#master-detail ul li a');
+        for (let index = 0; index < masterList.length; index++) {
+            if (ml === masterList[index].id) {
+                $('#' + masterList[index].id).css('color', '#483D8B')
+            } else {
+                $('#' + masterList[index].id).css('color', 'black')
+            }
+        }
+    }
+
+    // event merubah warna dan icon ketika salah satu navigasi dipilih
     $("dl dt a").on('click', function(e) {
+        detailList();
+        $('#' + this.masterSelected).css('color', 'black')
         this.targetSelect = e.target.id;
         idList = $("dl dt a");
         imgList = $("dl dt img");
         imgSelected = this.targetSelect + "-icon";
-        console.log(imgSelected);
         for (let index = 0; index < idList.length; index++) {
             if (idList[index].id === this.targetSelect) {
-                $('#' + idList[index].id).css('color', '#FF00FF');
+                $('#' + idList[index].id).css('color', '#BA55D3');
                 if (idList[index].id === 'master') {
                     $('#master-detail').toggle(500);
-
+                    $('.list-notes').toggle(500);
                 } else {
                     $('#master-detail').hide(500);
+                    $('.list-notes').show(500);
                 }
             } else {
                 $('#' + idList[index].id).css('color', 'black');
@@ -76,12 +167,25 @@
         }
 
         for (let index = 0; index < imgList.length; index++) {
-            console.log(imgList[index].classList[1])
             if (imgList[index].classList[1] === imgSelected) {
                 $('.' + imgList[index].classList[1]).attr('src', 'icon/' + imgSelected + '-onselect.svg');
             } else {
                 $('.' + imgList[index].classList[1]).attr('src', 'icon/' + imgList[index].classList[1] + '.svg');
             }
         }
+    });
+
+    // event merubah warna ketika salah satu button pada master list dipilih
+    $('#master-detail ul li').on('click', function(e) {
+        this.masterSelected = e.target.id;
+        detailList(this.masterSelected);
+    })
+
+    // modal notes
+    $('#button-notes').click(function() {
+        $('#modal-notes').fadeIn("slow");
+    });
+    $('#cancel-modal').click(function() {
+        $('#modal-notes').fadeOut("slow");
     });
 </script>
--- index.php
+++ index.php
@@ -1,23 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/style.css" />
-    <script src="js/dom.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-    <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
-    <title></title>
-  </head>
-  <body>
-    <div id="main-container">
-    </div>
-    <script>
-      $(document).ready(function () {
-        $("title").text('Login Tasnim Store')
-        $("#main-container").load("component-login/index.php");
-      });
-    </script>
-  </body>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="css/style.css" />
+  <script src="js/dom.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+  <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+  <title></title>
+</head>
+
+<body>
+  <div id="main-container">
+  </div>
+
+
+  <script>
+    $(document).ready(function() {
+      $("title").text('Login Tasnim Store')
+      $("#main-container").load("component-login/index.php");
+    });
+  </script>
+</body>
+
 </html>
