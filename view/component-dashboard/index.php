7ce77042a89cd67826b254710861fd4f2d1e9b7b
Giri Gimnastiar <girigimnastiarwork@gmail.com>
2022-12-09 08:39:19 +0700

    menambahkan dashboard

--- view/component-dashboard/index.php
+++ view/component-dashboard/index.php
@@ -1,7 +1,6 @@
 <div class="panel-dashboard">
     <div id="admin-board">
         <h5>Hello, <span>Giri Gimnastiar</span></h5>
-        <div class="imge">
-        </div>
+        <img src="images/icon-orang.png" alt="" class="imge">
     </div>
 </div>
